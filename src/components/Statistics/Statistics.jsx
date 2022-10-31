6d612d1c338f0ded2545fc75c4715f6d185714d2
Victor Nikolenko <victor1982nik@gmail.com>
2022-10-31 10:08:46 +0200

    mentor fix

--- src/components/Statistics/Statistics.jsx
+++ src/components/Statistics/Statistics.jsx
@@ -6,25 +6,26 @@ export function Statistics({ good, neutral, bad, total, positivePercentage }) {
   return (
     <>
       <ul>
-        <Paragraph>Good: {good}</Paragraph>        
+        <Paragraph>Good: {good}</Paragraph>
         <Paragraph>Neutral: {neutral}</Paragraph>
         <Paragraph>Bad: {bad}</Paragraph>
       </ul>
-      {total != 0 ? (
+      {total !== 0 ? (
         <>
           <Paragraph>Total: {total}</Paragraph>
-          <Paragraph>Positive feedback: {positivePercentage}%</Paragraph>          
+          <Paragraph>Positive feedback: {positivePercentage}%</Paragraph>
         </>
-      ) : <Notification message="There is no feedback"/>
-      }
+      ) : (
+        <Notification message="There is no feedback" />
+      )}
     </>
   );
 }
 
 Statistics.propTypes = {
-    good: PropTypes.number.isRequired,
-    neutral: PropTypes.number.isRequired,
-    bad: PropTypes.number.isRequired,
-    total: PropTypes.number.isRequired,
-    positivePercentage: PropTypes.number.isRequired
+  good: PropTypes.number.isRequired,
+  neutral: PropTypes.number.isRequired,
+  bad: PropTypes.number.isRequired,
+  total: PropTypes.number.isRequired,
+  positivePercentage: PropTypes.number.isRequired,
 };
